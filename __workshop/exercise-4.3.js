1ff07fafd8019f4c52c37bc04325a0e866cab2b7
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-07-14 20:51:17 -0400

    4.3

--- __workshop/exercise-4.3.js
+++ __workshop/exercise-4.3.js
@@ -1,5 +1,16 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 const getGeekJoke = async () => {
-  // ha hahahh
+  try {
+    const answer = await request({
+      uri: "https://geek-jokes.sameerkumar.website/api?format=json",
+      headers: { Accept: "application/json" },
+      json: true,
+    });
+    return answer.joke;
+  } catch (err) {
+    reject(err);
+  }
 };
+
+getGeekJoke.then((response) => console.log(response));
